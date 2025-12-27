fb1a01bd1719b173c71a17a669c7790163d3ce3f
Rohit-Sarkar55 <71070509+Rohit-Sarkar55@users.noreply.github.com>
2025-12-27 23:17:40 +0530

    Add RangeXorQueries.cpp for efficient range queries

---
+++ Range Queries/RangeXorQueries.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 100001;
+ 
+ 
+ 
+void solve(){
+    int n,q;
+    cin>>n>>q;
+ 
+    ll arr[n+1], pref[n+1];
+    for(int i=1;i <= n; i++){
+        cin>>arr[i];
+    }
+    pref[0] = 0;
+    for(int i=1;i<=n; i++) pref[i] = pref[i-1] ^ arr[i];
+ 
+    while(q--){
+        int x, y;
+        cin>>x>>y;
+        cout<<(pref[y] ^ pref[x-1])<<endl;
+       
+    }
+ 
+    
+}
+ 
+int main(){
+    
+ 
+    solve();
+    return 0;
+ 
+    
+}
